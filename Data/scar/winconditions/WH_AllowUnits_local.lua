2e1483707bba0a15765f6e5fc18c66cf3ceaae3b
omar\Col <omarsarakbi206@gmail.com>
2023-06-08 11:50:42 +0300

    scar and deprofile upload

---
+++ Data/scar/winconditions/WH_AllowUnits_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+exclusive = false,
+victory_condition = false,
+always_on = false,
+title = "[Witch Hunters] All Squads",
+win_message = "",
+lose_message = "",
+description = "If enabled, the Sororitas Redeemer, the Exalted Dreadnought and the Shield Maidens will ALWAYS be available. Otherwise, they will be available when direly needed.  (Better set it to [OFF])"
+}
---
+++ Data/scar/winconditions/disable_AI_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+	exclusive			= false,
+	victory_condition	= false,
+	always_on			= false,
+	title				= "Disable AI rule",
+	win_message 		= "$0",
+	lose_message  		= "$0",
+	description			= "Disables AI after 3 min of match, for developing purposes. Not for playing skirmish"
+}
